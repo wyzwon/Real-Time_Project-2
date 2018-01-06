8a9777d68a5d1f0a293772195bfe07581ff017c1
wyzwon <tjb5203@g.rit.edu>
2018-01-06 04:31:47 -0500

    changes to the scene are now stored in a buffer dictionary and applied all at once at the end of the cycle

--- src/server.js
+++ src/server.js
@@ -9,44 +9,31 @@ const port = process.env.PORT || process.env.NODE_PORT || 3000;
 // (in this case the same folder as the server js file)
 const index = fs.readFileSync(`${__dirname}/../client/index.html`);
 
-const sandObjectX = 100;
-const sandObjectY = 100;
-const sandObject = {}; // This is the master array of the sand data
+const sandArrayX = 150;
+const sandArrayY = 100;
+// const sandObject = {}; // This is the master array of the sand data
 
 // Instantiate the entire map as empty air cells
-const sandArray = new Array(sandObjectX);
+const sandArray = new Array(sandArrayX);
 for (let i = 0; i < sandArray.length; i++) {
-  sandArray[i] = new Array(sandObjectY);
+  sandArray[i] = new Array(sandArrayY);
   sandArray[i].fill(1);
 }
 
-// console.log(sandArray[5][5]);
-// console.log(blankBufferArray[5][5]);
-// sandArray[5][5] = "C";
-// console.log("-----------------");
-// console.log(sandArray[5][5]);
-// console.log(blankBufferArray[5][5]);
+let changeBuffer = {};
+changeBuffer = {};
 
 // Instantiate an array of empty cells to reset the changeBufferArray with.
-const blankBufferArray = new Array(sandObjectX);
+const blankBufferArray = new Array(sandArrayX);
 for (let i = 0; i < sandArray.length; i++) {
-  blankBufferArray[i] = new Array(sandObjectY);
+  blankBufferArray[i] = new Array(sandArrayY);
   blankBufferArray[i].fill(0);
   // blankBufferArray[i].fill(Math.floor(Math.random() * (5)));
 }
 
+// https://stackoverflow.com/questions/25492329/is-array-slice-enough-to-handle-a-multidimensional-array-in-javascript
 // Stores requested changes between consolidations and is instantiated and reset to all 0 values
 const changeBufferArray = blankBufferArray.map(row => row.slice());
-
-for (let lx = 0; lx < sandObjectX; lx += 1) {
-  sandObject[lx] = {};
-  for (let ly = 0; ly < sandObjectY; ly += 1) {
-    sandObject[lx][ly] = 1;
-    // sandObject[lx][ly] = Math.floor(Math.random() * (5));
-  }
-}
-// blankBufferArray = Object.create(sandObject);
-// changeBufferArray = Object.create(sandObject);
 
 
 const onRequest = (request, response) => {
@@ -69,11 +56,8 @@ const onJoined = (sock) => {
     socket.join('room1');
     console.log(`${socket.id} joined room1`);
 
-    //socket.emit('fullArray', { value: sandObject });
-	socket.emit('fullArray', { value: sandArray });
-
-    // Send the current map data to the new user (try this and send as object if it doesnt work)
-    // socket.emit('fullArray', { value: sandArray });
+    // Send the current map data to the new user along with the suggested size of pixels
+    socket.emit('setUp', { value: sandArray, suggestedPixelSize: 4 });
   });
 };
 
@@ -105,11 +89,19 @@ const onArrayUpdateToServer = (sock) => {
       const tile = data[changeListKeys[i]];
 
       // Assign the tile to the corresponding spot if it exists.
-      if ((tile.x < sandObjectX) && (tile.y < sandObjectY)) {
-        sandObject[tile.x][tile.y] = tile.type;
+      if ((tile.x < sandArrayX) && (tile.y < sandArrayY)) {
+        // save the changes to the server copy
+        sandArray[tile.x][tile.y] = tile.type;
+
+        // add the changes to the changeBufferArray
         changeBufferArray[tile.x][tile.y] = tile.type;
 
+        // add the changes to the changeBuffer
+        changeBuffer[`${tile.x},${tile.y}`] = { x: tile.x, y: tile.y, type: tile.type };
+
+        // temporarily broadcast changes immediately
         io.sockets.in('room1').emit('arrayUpdates', data);
+		
       }
     }
   });
@@ -144,8 +136,24 @@ setInterval(() => {
   tilesArray[0] = { x: 2, y: 67, type: Math.floor(Math.random() * (5)) };
   tilesArray[1] = { x: 30, y: 22, type: Math.floor(Math.random() * (5)) };
 
-  // console.log(time);
   io.sockets.in('room1').emit('arrayUpdates', tilesArray);
+
+  const bufferListKeys = Object.keys(changeBuffer);
+
+  // place each change in the local copy
+  for (let i = 0; i < bufferListKeys.length; i++) {
+    const tile = changeBuffer[bufferListKeys[i]];
+
+    // Assign the tile to the corresponding spot if it exists.
+    if ((tile.x < sandArrayX) && (tile.y < sandArrayY)) {
+      // save the changes to the server copy
+      sandArray[tile.x][tile.y] = tile.type;
+    }
+  }
+  
+  //console.log(sandArray);
+  // Clear the buffer for use in next cycle
+  changeBuffer = {};
 }, 300);
 
 console.log('Websocket server started');
